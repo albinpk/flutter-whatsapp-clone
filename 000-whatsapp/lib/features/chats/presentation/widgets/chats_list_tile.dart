fe9715a43b37378e5ec22e56315be0afa5d5bcef
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-08-25 19:16:36 +0530

    refactor(whatsapp): update `ChatListTile`
    remove trailing widget,
    and use Row with Expanded in title and subtitle

--- 000-whatsapp/lib/features/chats/presentation/widgets/chats_list_tile.dart
+++ 000-whatsapp/lib/features/chats/presentation/widgets/chats_list_tile.dart
@@ -6,10 +6,50 @@ class ChatsListTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: const Text('John Doe'),
-      subtitle: const Text('Hi there!'),
       leading: const CircleAvatar(radius: 26),
-      trailing: const Text('8/8/22'),
+      title: Row(
+        children: [
+          Expanded(
+            child: Text(
+              'John Doe',
+              style: Theme.of(context)
+                  .textTheme
+                  .titleMedium!
+                  .copyWith(fontWeight: FontWeight.w500),
+            ),
+          ),
+          Text(
+            '8/8/22',
+            style: Theme.of(context).textTheme.bodySmall,
+          ),
+        ],
+      ),
+      subtitle: Padding(
+        padding: const EdgeInsets.only(top: 5),
+        child: Row(
+          children: [
+            const Expanded(
+              child: Text(
+                'Hi there!',
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+            IconTheme(
+              data: IconTheme.of(context).copyWith(
+                size: 18,
+                color: Colors.grey[600],
+              ),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: const [
+                  Icon(Icons.volume_off),
+                  Icon(Icons.push_pin),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
       onTap: () {},
     );
   }
