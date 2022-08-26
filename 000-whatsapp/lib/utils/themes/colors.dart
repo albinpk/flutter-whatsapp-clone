d3be66d570eceadbe9908e5cb529f1384229098b
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-08-26 17:15:08 +0530

    refactor(whatsapp): create colors.dart
    create light and dark theme colors

---
+++ 000-whatsapp/lib/utils/themes/colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import 'custom_colors.dart';
+
+const lightThemeColors = CustomColors(
+  chatsListTileTitle: Color(0xFF111B21),
+  chatsListTileSubtitle: Color(0xFF667781),
+  chatsListTileIcon: Color(0xFF8696A0),
+  chatsListTileBadge: Color(0xFF25D366),
+);
+
+const darkThemeColors = CustomColors(
+  chatsListTileTitle: Color(0xFFE9EDEF),
+  chatsListTileSubtitle: Color(0xFF8696A0),
+  chatsListTileIcon: Color(0xFF888D90),
+  chatsListTileBadge: Color(0xFF00A884),
+);
