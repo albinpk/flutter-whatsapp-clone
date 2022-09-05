9556110e3bc2df0e4019b3a463c0dd680e9260fc
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-09-05 21:01:16 +0530

    refactor(whatsapp): `PlatformType` extension
    check TargetPlatform is native android/ios/fuchsia

--- 000-whatsapp/lib/utils/extensions/platform_type.dart
+++ 000-whatsapp/lib/utils/extensions/platform_type.dart
@@ -1,15 +1,13 @@
 import 'package:flutter/foundation.dart';
 
 extension PlatformType on TargetPlatform {
-  /// Return `true` if [TargetPlatform] is linux, macos or windows.
-  bool get isDesktop => const <TargetPlatform>{
-        TargetPlatform.linux,
-        TargetPlatform.macOS,
-        TargetPlatform.windows,
-      }.contains(this);
+  /// Return `true` if is web or [TargetPlatform] is linux, macos or windows.
+  bool get isDesktop => !isMobile;
 
-  /// Return `true` if [TargetPlatform] is android, ios or fuchsia.
-  bool get isMobile => const <TargetPlatform>{
+  /// Return `true` if [TargetPlatform] is (native) android, ios or fuchsia.
+  bool get isMobile =>
+      !kIsWeb &&
+      const <TargetPlatform>{
         TargetPlatform.android,
         TargetPlatform.iOS,
         TargetPlatform.fuchsia,
