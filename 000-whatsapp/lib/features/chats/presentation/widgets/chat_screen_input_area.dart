61e5f7a7fa383e1a432bb2e2a8df517e6c60fe4b
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-09-06 15:18:34 +0530

    refactor(whatsapp): create input area for mobile and desktop

--- 000-whatsapp/lib/features/chats/presentation/widgets/chat_screen_input_area.dart
+++ 000-whatsapp/lib/features/chats/presentation/widgets/chat_screen_input_area.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
 
+import '../../../../utils/extensions/platform_type.dart';
 import '../../../../utils/themes/custom_colors.dart';
 
 class ChatScreenInputArea extends StatelessWidget {
   const ChatScreenInputArea({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Theme.of(context).platform.isMobile
+        ? const _MobileInputArea()
+        : const _DesktopInputArea();
+  }
+}
+
+class _MobileInputArea extends StatelessWidget {
+  const _MobileInputArea({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -65,22 +77,72 @@ class ChatScreenInputArea extends StatelessWidget {
   }
 }
 
+class _DesktopInputArea extends StatelessWidget {
+  const _DesktopInputArea({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: kBottomNavigationBarHeight + 5,
+      child: ColoredBox(
+        color: const Color(0xFF202C33),
+        child: Padding(
+          padding: const EdgeInsets.all(8),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.emoji_emotions_rounded),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.attach_file),
+              ),
+              Expanded(
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: const ColoredBox(
+                    color: Color(0xFF2A3942),
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 10),
+                      child: _InputField(),
+                    ),
+                  ),
+                ),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.mic),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
 class _InputField extends StatelessWidget {
   const _InputField({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final isMobile = Theme.of(context).platform.isMobile;
+    final textTheme = Theme.of(context).textTheme;
+
     return TextField(
-      style: Theme.of(context).textTheme.titleMedium!.copyWith(
-            color: Colors.white,
-            fontSize: 18,
-          ),
+      style: textTheme.titleMedium!.copyWith(
+        fontSize: isMobile ? 18 : null,
+        fontWeight: isMobile ? null : FontWeight.w300,
+      ),
       textCapitalization: TextCapitalization.sentences,
-      decoration: const InputDecoration(
-        hintText: 'Message',
+      decoration: InputDecoration(
+        hintText: isMobile ? 'Message' : ' Type a message',
         border: InputBorder.none,
       ),
-      cursorColor: CustomColors.of(context).chatsListTileBadge,
+      cursorColor:
+          isMobile ? CustomColors.of(context).chatsListTileBadge : Colors.white,
     );
   }
 }
