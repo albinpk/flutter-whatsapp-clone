9ba692fef9e41ed9732bd15abbaa7a9e897c15e0
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-09-06 17:44:23 +0530

    refactor(whatsapp): add friends field in `User`

--- 000-whatsapp/lib/models/user_model.dart
+++ 000-whatsapp/lib/models/user_model.dart
@@ -1,9 +1,11 @@
 class User {
   final int id;
   final String name;
+  final List<int> friends;
 
   const User({
     required this.id,
     required this.name,
+    this.friends = const [],
   });
 }
