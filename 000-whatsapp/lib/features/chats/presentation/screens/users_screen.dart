f8f944e55cfd7b6aaf3485a357de0d9787f7d3d6
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-09-12 14:46:23 +0530

    refactor(whatsapp): extract AppBar from UsersScreen
    change preferredSize for desktop

--- 000-whatsapp/lib/features/chats/presentation/screens/users_screen.dart
+++ 000-whatsapp/lib/features/chats/presentation/screens/users_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../../utils/extensions/platform_type.dart';
 import '../../bloc/chats_bloc.dart';
 import '../views/users_view.dart';
 
@@ -15,27 +16,55 @@ class UsersScreen extends StatelessWidget {
         return false;
       },
       child: Scaffold(
-        appBar: AppBar(
-          leading: IconButton(
-            onPressed: () {
-              context.read<ChatsBloc>().add(const ChatsContactsScreenPopped());
-            },
-            icon: const Icon(Icons.arrow_back),
-          ),
-          title: const Text('Select contact'),
-          actions: [
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(Icons.search),
-            ),
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(Icons.more_vert),
-            ),
-          ],
-        ),
+        appBar: _AppBar(isMobile: Theme.of(context).platform.isMobile),
         body: const UsersView(),
       ),
     );
   }
+}
+
+class _AppBar extends StatelessWidget with PreferredSizeWidget {
+  const _AppBar({
+    Key? key,
+    required this.isMobile,
+  }) : super(key: key);
+
+  final bool isMobile;
+
+  @override
+  Widget build(BuildContext context) {
+    final appBar = AppBar(
+      leading: IconButton(
+        onPressed: () {
+          context.read<ChatsBloc>().add(const ChatsContactsScreenPopped());
+        },
+        icon: const Icon(Icons.arrow_back),
+      ),
+      title: Text(isMobile ? 'Select contact' : 'New chat'),
+      actions: !isMobile
+          ? null
+          : [
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.search),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.more_vert),
+              ),
+            ],
+    );
+
+    return isMobile
+        ? appBar
+        : AppBar(
+            bottom: appBar,
+            automaticallyImplyLeading: false,
+          );
+  }
+
+  @override
+  Size get preferredSize => isMobile
+      ? const Size.fromHeight(kToolbarHeight)
+      : const Size.fromHeight(kToolbarHeight * 2);
 }
