11208f9f77b840c04e9b5e0efb563939966c27a7
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-09-14 16:38:17 +0530

    refactor(whatsapp): create `WhatsAppUser` model

---
+++ 000-whatsapp/lib/models/whats_app_user_model.dart
@@ -0,0 +1,33 @@
+import 'package:equatable/equatable.dart';
+import 'package:uuid/uuid.dart';
+
+import 'contact_model.dart';
+
+const _uuid = Uuid();
+
+class WhatsAppUser extends Equatable {
+  final String id;
+  final String name;
+  final String phNumber;
+  final String about;
+  final List<String> friends;
+
+  const WhatsAppUser({
+    required this.id,
+    required this.name,
+    required this.phNumber,
+    this.friends = const [],
+    this.about = 'About',
+  });
+
+  factory WhatsAppUser.fromContact(Contact contact) {
+    return WhatsAppUser(
+      id: _uuid.v4(),
+      name: contact.name,
+      phNumber: contact.phNumber,
+    );
+  }
+
+  @override
+  List<Object> get props => [name, phNumber, about];
+}
