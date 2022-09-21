e40eadb03bcbc6f7c4c4c7d1b0d023f8d0664293
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-09-21 11:29:13 +0530

    refactor(whatsapp): create `SearchBar` widget

--- 000-whatsapp/lib/features/chat/features/recent_chats/presentation/widgets/recent_chats_app_bar_desktop.dart
+++ 000-whatsapp/lib/features/chat/features/recent_chats/presentation/widgets/recent_chats_app_bar_desktop.dart
@@ -41,6 +41,67 @@ class RecentChatsAppBarDesktop extends StatelessWidget
           padding: actionsIconPadding,
         ),
       ],
+      bottom: const _SearchBar(),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight * 2);
+}
+
+class _SearchBar extends StatelessWidget with PreferredSizeWidget {
+  const _SearchBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final customColors = CustomColors.of(context);
+    return SizedBox(
+      height: kToolbarHeight,
+      child: ColoredBox(
+        color: customColors.background!,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 8),
+          child: Row(
+            children: [
+              Expanded(
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: ColoredBox(
+                    color: customColors.secondary!,
+                    child: Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 15),
+                          child: Icon(
+                            Icons.search,
+                            color: customColors.iconMuted,
+                          ),
+                        ),
+                        const Expanded(
+                          child: TextField(
+                            cursorColor: Colors.black,
+                            cursorWidth: 1,
+                            decoration: InputDecoration(
+                              hintText: 'Search or start new chat',
+                              contentPadding: EdgeInsets.only(bottom: 10),
+                              border: InputBorder.none,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              IconButton(
+                onPressed: () {},
+                color: customColors.iconMuted,
+                icon: const Icon(Icons.sort),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 
