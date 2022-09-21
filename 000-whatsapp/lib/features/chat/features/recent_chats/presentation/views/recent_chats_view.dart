4d1acd994ad0f62faf8d0d5856af900bbaf62477
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-09-21 17:33:54 +0530

    refactor(whatsapp): close chat search on scroll

--- 000-whatsapp/lib/features/chat/features/recent_chats/presentation/views/recent_chats_view.dart
+++ 000-whatsapp/lib/features/chat/features/recent_chats/presentation/views/recent_chats_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../../../../models/app_user_model.dart';
 import '../../../../../../utils/extensions/platform_type.dart';
+import '../../../search/search.dart';
 import '../widgets/widgets.dart';
 
 class RecentChatsView extends StatelessWidget {
@@ -16,11 +17,42 @@ class RecentChatsView extends StatelessWidget {
   }
 }
 
-class _RecentChatsViewMobile extends StatelessWidget {
+class _RecentChatsViewMobile extends StatefulWidget {
   const _RecentChatsViewMobile({Key? key}) : super(key: key);
 
   @override
+  State<_RecentChatsViewMobile> createState() => _RecentChatsViewMobileState();
+}
+
+class _RecentChatsViewMobileState extends State<_RecentChatsViewMobile> {
+  late ScrollController _scrollController;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _scrollController = PrimaryScrollController.of(context)!;
+    final state = context.read<ChatSearchBloc>().state;
+    if (state is ChatSearchOpenState) {
+      _scrollController.addListener(_scrollListener);
+    }
+  }
+
+  void _scrollListener() {
+    context.read<ChatSearchBloc>().add(const ChatSearchClose());
+    _scrollController.removeListener(_scrollListener);
+  }
+
+  @override
+  void dispose() {
+    _scrollController.removeListener(_scrollListener);
+    // Don't dispose _scrollController.
+    // because it is NestedScrollView's controller
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    context.select((ChatSearchBloc bloc) => bloc.state is ChatSearchOpenState);
     return SafeArea(
       top: false,
       bottom: false,
