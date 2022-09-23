9d497fd74a471325827201f761317d71f3fe58d5
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-09-23 20:39:52 +0530

    refactor(whatsapp): change FAB icon when tab view change
    also change onPressed handler

--- 000-whatsapp/lib/features/home_screen/presentation/widgets/fab.dart
+++ 000-whatsapp/lib/features/home_screen/presentation/widgets/fab.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../chat/chat.dart';
+import '../../home_screen.dart';
 
+/// FloatingActionButton
 class FAB extends StatelessWidget {
   const FAB({Key? key}) : super(key: key);
 
@@ -15,9 +17,27 @@ class FAB extends StatelessWidget {
     if (isSearchOpen) return const SizedBox.shrink();
 
     return FloatingActionButton(
-      child: const Icon(Icons.message),
+      child: BlocSelector<TabViewBloc, TabViewState, TabView>(
+        selector: (state) => state.tabView,
+        builder: (context, tabView) {
+          switch (tabView) {
+            case TabView.chats:
+              return const Icon(Icons.message);
+            case TabView.status:
+              return const Icon(Icons.camera_alt);
+            case TabView.calls:
+              return const Icon(Icons.add_call);
+          }
+        },
+      ),
       onPressed: () {
-        context.read<NewChatBloc>().add(const NewChatSelectionScreenOpen());
+        switch (context.read<TabViewBloc>().state.tabView) {
+          case TabView.chats:
+            context.read<NewChatBloc>().add(const NewChatSelectionScreenOpen());
+            break;
+          case TabView.status:
+          case TabView.calls:
+        }
       },
     );
   }
