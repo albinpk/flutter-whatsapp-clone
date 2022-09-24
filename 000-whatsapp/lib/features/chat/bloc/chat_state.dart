01d7da8b44e55f73013395dd2304cb45dfc1ebe3
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-09-24 14:54:29 +0530

    refactor(whatsapp): create `recentChats` getter in ChatState
    return chats sorted by last message date

--- 000-whatsapp/lib/features/chat/bloc/chat_state.dart
+++ 000-whatsapp/lib/features/chat/bloc/chat_state.dart
@@ -6,6 +6,21 @@ class ChatState extends Equatable {
 
   const ChatState.initial(this._messageStore);
 
+  /// List of chats sorted by last message date
+  List<RecentChat> get recentChats {
+    return _messageStore.entries
+        .map<RecentChat>(
+          (e) => RecentChat(
+            user: whatsappUsers.singleWhere((u) => u.id == e.key),
+            lastMessage: e.value.last,
+          ),
+        )
+        .toList()
+      ..sort(
+        (a, b) => a.lastMessage.time.compareTo(b.lastMessage.time),
+      );
+  }
+
   @override
   List<Object> get props => [_messageStore];
 }
