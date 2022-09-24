76ff3c0219feb8b1b89eac0491aa3fd9fd04e989
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-09-24 17:21:33 +0530

    refactor(whatsapp): add event on text input change

--- 000-whatsapp/lib/features/chat/features/chat_room/presentation/widgets/chat_text_field.dart
+++ 000-whatsapp/lib/features/chat/features/chat_room/presentation/widgets/chat_text_field.dart
@@ -1,10 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../../../../core/utils/extensions/platform_type.dart';
 import '../../../../../../core/utils/themes/custom_colors.dart';
+import '../../../../chat.dart';
 
-class ChatTextField extends StatelessWidget {
+class ChatTextField extends StatefulWidget {
   const ChatTextField({Key? key}) : super(key: key);
+
+  @override
+  State<ChatTextField> createState() => _ChatTextFieldState();
+}
+
+class _ChatTextFieldState extends State<ChatTextField> {
+  final _textEditingController = TextEditingController();
+  late final _chatRoomBloc = context.read<ChatRoomBloc>();
+
+  @override
+  void initState() {
+    super.initState();
+    _textEditingController.addListener(_chatTextFieldListener);
+  }
+
+  void _chatTextFieldListener() {
+    _chatRoomBloc.add(
+      ChatRoomTextInputValueChange(text: _textEditingController.text),
+    );
+  }
+
+  @override
+  void dispose() {
+    _textEditingController.removeListener(_chatTextFieldListener);
+    _textEditingController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,6 +41,7 @@ class ChatTextField extends StatelessWidget {
     final textTheme = Theme.of(context).textTheme;
 
     return TextField(
+      controller: _textEditingController,
       style: textTheme.titleMedium!.copyWith(
         fontSize: isMobile ? 18 : null,
         fontWeight: isMobile
