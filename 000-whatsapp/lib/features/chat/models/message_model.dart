93e34a736a47a6d398ef38706f0d3332e0c65cec
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-09-25 11:46:35 +0530

    refactor(whatsapp): create `Message.fromText` constructor

--- 000-whatsapp/lib/features/chat/models/message_model.dart
+++ 000-whatsapp/lib/features/chat/models/message_model.dart
@@ -1,4 +1,5 @@
 import 'package:equatable/equatable.dart';
+import 'package:uuid/uuid.dart';
 
 import '../../../core/models/models.dart';
 
@@ -21,6 +22,14 @@ class Message extends Equatable {
     required this.time,
     required this.author,
   });
+
+  /// Create [Message] from [MessageContent.text]
+  Message.fromText(
+    String text, {
+    required this.author,
+  })  : id = const Uuid().v4(),
+        content = MessageContent(text: text),
+        time = DateTime.now();
 
   @override
   List<Object> get props => [id];
