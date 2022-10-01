9fc0d9c73507da3c7c4bae248d8a977f970a42ea
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-10-01 15:14:05 +0530

    refactor(whatsapp): wrap message box with `VisibilityDetector`
    update message status on visibility change

--- 000-whatsapp/lib/features/chat/features/chat_room/presentation/views/chat_room_messages_view.dart
+++ 000-whatsapp/lib/features/chat/features/chat_room/presentation/views/chat_room_messages_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:visibility_detector/visibility_detector.dart';
 
 import '../../../../../../core/models/models.dart';
 import '../../../../chat.dart';
@@ -9,7 +10,8 @@ class ChatRoomMessagesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final selectedUser = context.select((WhatsAppUser user) => user);
+    final selectedUser = context.watch<WhatsAppUser>();
+    // Messages with currently selected user
     final messages = context
         .select((ChatBloc bloc) => bloc.state.getMessages(selectedUser))
         .reversed
@@ -25,10 +27,36 @@ class ChatRoomMessagesView extends StatelessWidget {
         final isFirstInSection = index == length - 1
             ? true
             : messages[++index].author != message.author;
-        return MessageBox(
+
+        // The message box
+        final messageBox = MessageBox(
           message: message,
           isFirstInSection: isFirstInSection,
         );
+
+        // Wrap the message box with VisibilityDetector if the message
+        // is incoming message and its status in delivered.
+        // And trigger ChatMarkMessageAsRead event when the message box is fully visible.
+        if (message.author == selectedUser &&
+            message.status == MessageStatus.delivered) {
+          return VisibilityDetector(
+            key: Key(message.id),
+            onVisibilityChanged: (info) {
+              if (info.visibleFraction == 1) {
+                context.read<ChatBloc>().add(
+                      ChatMarkMessageAsRead(
+                        user: selectedUser,
+                        message: message,
+                      ),
+                    );
+              }
+            },
+            child: messageBox,
+          );
+        }
+
+        // No VisibilityDetector if the message is already read.
+        return messageBox;
       },
     );
   }
