6123312b20f5a0797e95ef03985db5bd22c0b3a1
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-10-06 00:45:38 +0530

    refactor(whatsapp): remove CircleAvatar from `UserDP`
    because, cannot use for own animations, its already a animated widget

--- 000-whatsapp/lib/core/widgets/user_dp.dart
+++ 000-whatsapp/lib/core/widgets/user_dp.dart
@@ -11,15 +11,20 @@ class UserDP extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final isLight = Theme.of(context).brightness == Brightness.light;
-    return CircleAvatar(
-      radius: radius,
-      backgroundColor: isLight ? Colors.white : const Color(0xFFCED8DE),
-      child: FractionalTranslation(
-        translation: const Offset(-0.11, -0.11),
-        child: Icon(
-          Icons.account_circle,
-          color: isLight ? const Color(0xFFCED8DE) : const Color(0xFF627884),
-          size: radius * 2.45,
+    return ClipOval(
+      child: SizedBox.square(
+        dimension: radius * 2,
+        child: ColoredBox(
+          color: isLight ? Colors.white : const Color(0xFFCED8DE),
+          child: FractionalTranslation(
+            translation: const Offset(-0.11, -0.11),
+            child: Icon(
+              Icons.account_circle,
+              color:
+                  isLight ? const Color(0xFFCED8DE) : const Color(0xFF627884),
+              size: radius * 2.45,
+            ),
+          ),
         ),
       ),
     );
