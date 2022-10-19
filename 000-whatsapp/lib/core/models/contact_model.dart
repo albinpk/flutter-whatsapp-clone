c1b17c2b224efefe28b4187cc913ecce1692597c
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-10-19 21:51:19 +0530

    docs(whatsapp): add comments in `WhatsAppUser` and `Contact`

--- 000-whatsapp/lib/core/models/contact_model.dart
+++ 000-whatsapp/lib/core/models/contact_model.dart
@@ -1,9 +1,14 @@
 import 'package:equatable/equatable.dart';
 
+/// Contact in user's phone.
 class Contact extends Equatable {
+  /// Contact name.
   final String name;
+
+  /// Phone number.
   final String phNumber;
 
+  /// Create a contact with [name] and [phNumber].
   const Contact({
     required this.name,
     required this.phNumber,
--- 000-whatsapp/lib/core/models/whats_app_user_model.dart
+++ 000-whatsapp/lib/core/models/whats_app_user_model.dart
@@ -5,12 +5,21 @@ import 'contact_model.dart';
 
 const _uuid = Uuid();
 
+/// WhatsApp user model.
 class WhatsAppUser extends Equatable {
+  /// User id.
   final String id;
+
+  /// User name.
   final String name;
+
+  /// Phone number of user.
   final String phNumber;
+
+  /// User about text.
   final String about;
 
+  /// Create WhatsApp user.
   const WhatsAppUser({
     required this.id,
     required this.name,
@@ -18,12 +27,13 @@ class WhatsAppUser extends Equatable {
     this.about = 'About',
   });
 
+  /// Create WhatsApp user from [Contact].
   factory WhatsAppUser.fromContact(Contact contact) {
     return WhatsAppUser(
-      id: _uuid.v4(),
-      name: contact.name,
-      phNumber: contact.phNumber,
-    );
+          id: _uuid.v4(),
+          name: contact.name,
+          phNumber: contact.phNumber,
+        );
   }
 
   @override
