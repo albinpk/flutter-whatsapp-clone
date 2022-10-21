cc60b559de63b164b60337ff6c39bc5a8dc54905
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-10-21 21:44:36 +0530

    feat(whatsapp): create status progress animation
    start progress bar animation on initState and
    pop StatusScreen when animation completed.
    
    add GestureDetector to detect tap and long tap.
    pop the screen on tap.
    stop the animation on long tap

--- 000-whatsapp/lib/features/status/presentation/screens/status_screen.dart
+++ 000-whatsapp/lib/features/status/presentation/screens/status_screen.dart
@@ -1,10 +1,12 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 import '../../../../core/widgets/widgets.dart';
 import '../../status.dart';
 
-class StatusScreen extends StatelessWidget {
+class StatusScreen extends StatefulWidget {
   const StatusScreen({
     super.key,
     required this.status,
@@ -13,14 +15,89 @@ class StatusScreen extends StatelessWidget {
   final Status status;
 
   @override
+  State<StatusScreen> createState() => _StatusScreenState();
+}
+
+class _StatusScreenState extends State<StatusScreen>
+    with SingleTickerProviderStateMixin {
+  /// Controller for status progress bar animation.
+  late final _controller = AnimationController(
+    vsync: this,
+    duration: const Duration(seconds: 5),
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    _controller
+      ..forward()
+      ..addStatusListener(_animationStatusListener);
+  }
+
+  /// Pop the screen when animation completed.
+  void _animationStatusListener(AnimationStatus status) {
+    if (status == AnimationStatus.completed) {
+      Navigator.of(context).pop();
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller.removeStatusListener(_animationStatusListener);
+    _controller.dispose();
+    _timer?.cancel();
+    super.dispose();
+  }
+
+  /// Timer for detect normal and long tap.
+
+  // Pop the screen on normal tap.
+  // If it is long tap then stop the animation,
+  // and forward it when tapUp on tapCancel detected.
+  Timer? _timer;
+  bool _isLong = false;
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       extendBodyBehindAppBar: true,
-      appBar: _AppBar(status: status),
-      body: Center(
-        child: Image.network(status.content.imgUrl!),
+      appBar: _AppBar(
+        status: widget.status,
+        animation: _controller,
+      ),
+      body: GestureDetector(
+        behavior: HitTestBehavior.opaque,
+        onTapDown: _onTapDown,
+        onTapUp: _onTapUpOrCancel,
+        onTapCancel: _onTapUpOrCancel,
+
+        // Status image
+        child: Center(
+          child: Image.network(widget.status.content.imgUrl!),
+        ),
       ),
     );
+  }
+
+  // Stop the animation and start the timer.
+  void _onTapDown(_) {
+    _controller.stop();
+    _timer = Timer(
+      const Duration(milliseconds: 100),
+      () => _isLong = true,
+    );
+  }
+
+  // Forward the animation if its long tap,
+  // otherwise pop the screen.
+  void _onTapUpOrCancel([_]) {
+    if (_isLong) {
+      _controller.forward();
+      _isLong = false;
+    } else {
+      _timer!.cancel();
+      Navigator.of(context).pop();
+    }
   }
 }
 
@@ -28,9 +105,13 @@ class _AppBar extends StatelessWidget implements PreferredSizeWidget {
   const _AppBar({
     Key? key,
     required this.status,
+    required this.animation,
   }) : super(key: key);
 
   final Status status;
+
+  /// Progress bar animation.
+  final Animation<double> animation;
 
   @override
   Widget build(BuildContext context) {
@@ -46,15 +127,23 @@ class _AppBar extends StatelessWidget implements PreferredSizeWidget {
               borderRadius: BorderRadius.circular(5),
               child: Stack(
                 fit: StackFit.expand,
-                children: const [
+                children: [
                   // Status progress bar background
-                  ColoredBox(color: Colors.white30),
+                  const ColoredBox(color: Colors.white30),
                   // Status progress bar
-                  FractionallySizedBox(
-                    alignment: Alignment.centerLeft,
-                    heightFactor: 1,
-                    widthFactor: 0.5,
-                    child: ColoredBox(color: Colors.white),
+                  AnimatedBuilder(
+                    animation: animation,
+                    builder: (context, child) {
+                      return FractionallySizedBox(
+                        alignment: Alignment.centerLeft,
+                        heightFactor: 1,
+                        widthFactor: animation.value,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(5),
+                          child: const ColoredBox(color: Colors.white),
+                        ),
+                      );
+                    },
                   ),
                 ],
               ),
