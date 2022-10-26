faecb9c701c0f862c10f04512e76de524146aa8e
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-10-26 16:34:50 +0530

    refactor(whatsapp): create `StatusTile.fromStatus` constructor

--- 000-whatsapp/lib/features/status/presentation/widgets/status_tile.dart
+++ 000-whatsapp/lib/features/status/presentation/widgets/status_tile.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 import '../../../../core/utils/themes/custom_colors.dart';
+import '../../status.dart';
 
 class StatusTile extends StatelessWidget {
   /// Create a status tile widget.
@@ -10,18 +12,36 @@ class StatusTile extends StatelessWidget {
     required this.title,
     required this.subtitle,
     required this.onTap,
-  });
+  })  : assert(onTap != null),
+        _status = null;
+
+  /// Create status tile from [Status].
+  StatusTile.fromStatus(Status status, {super.key})
+      : _status = status,
+        leading = StatusPreviewCircle(status: status),
+        title = status.author.name,
+        subtitle = DateFormat(DateFormat.HOUR_MINUTE).format(status.time),
+        onTap = null;
 
   final Widget leading;
   final String title;
   final String subtitle;
-  final VoidCallback onTap;
+  final VoidCallback? onTap;
+
+  final Status? _status;
 
   @override
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
     return ListTile(
-      onTap: onTap,
+      onTap: onTap ??
+          () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => StatusScreen(status: _status!),
+              ),
+            );
+          },
       leading: leading,
       title: Text(
         title,
