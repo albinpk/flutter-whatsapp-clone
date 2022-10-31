e6d98e39dd67e26b688bcfdb18deea45c3b3d7f2
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-10-31 20:58:39 +0530

    refactor(whatsapp): set minimum window size for desktop

--- 000-whatsapp/lib/main.dart
+++ 000-whatsapp/lib/main.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:window_size/window_size.dart';
 
 import 'core/utils/bloc/global_bloc_observer.dart';
+import 'core/utils/extensions/target_platform.dart';
 import 'whats_app.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (!kIsWeb && defaultTargetPlatform.isDesktop) {
+    setWindowTitle('WhatsApp');
+    setWindowMinSize(const Size(500, 400));
+  }
+
   if (kDebugMode) Bloc.observer = GlobalBlocObserver();
 
   runApp(const WhatsApp());
