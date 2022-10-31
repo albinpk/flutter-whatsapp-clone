851e44f411be1e2ab2b208754203dc137c59cce7
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-10-31 17:30:44 +0530

    refactor(whatsapp): separate `StatusScreen` for mobile and desktop
    create named constructor in _AppBar for mobile and desktop.
    create ValueNotifier<bool> _isAnimating to
    update play/pause icon in appBar desktop.

--- 000-whatsapp/lib/features/status/presentation/screens/status_screen.dart
+++ 000-whatsapp/lib/features/status/presentation/screens/status_screen.dart
@@ -3,10 +3,11 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/intl.dart';
 import 'package:visibility_detector/visibility_detector.dart';
 
+import '../../../../core/utils/extensions/target_platform.dart';
 import '../../../../core/widgets/widgets.dart';
 import '../../status.dart';
 
-class StatusScreen extends StatefulWidget {
+class StatusScreen extends StatelessWidget {
   const StatusScreen({
     super.key,
     required this.status,
@@ -21,10 +22,32 @@ class StatusScreen extends StatefulWidget {
   final PageController pageController;
 
   @override
-  State<StatusScreen> createState() => _StatusScreenState();
+  Widget build(BuildContext context) {
+    return Theme.of(context).platform.isMobile
+        ? _StatusScreenMobile(status: status, pageController: pageController)
+        : _StatusScreenDesktop(status: status, pageController: pageController);
+  }
 }
 
-class _StatusScreenState extends State<StatusScreen>
+class _StatusScreenMobile extends StatefulWidget {
+  const _StatusScreenMobile({
+    Key? key,
+    required this.status,
+    required this.pageController,
+  }) : super(key: key);
+
+  final Status status;
+
+  /// The [StatusPageView] controller.
+  ///
+  /// To stop animation on page scroll.
+  final PageController pageController;
+
+  @override
+  State<_StatusScreenMobile> createState() => _StatusScreenMobileState();
+}
+
+class _StatusScreenMobileState extends State<_StatusScreenMobile>
     with SingleTickerProviderStateMixin {
   /// Controller for status progress bar animation.
   late final _controller = AnimationController(
@@ -124,7 +147,7 @@ class _StatusScreenState extends State<StatusScreen>
               ),
             );
           },
-          child: _AppBar(
+          child: _AppBar.mobile(
             status: widget.status,
             animation: _controller,
           ),
@@ -162,53 +185,253 @@ class _StatusScreenState extends State<StatusScreen>
   }
 }
 
+class _StatusScreenDesktop extends StatefulWidget {
+  const _StatusScreenDesktop({
+    Key? key,
+    required this.status,
+    required this.pageController,
+  }) : super(key: key);
+
+  final Status status;
+
+  /// The [StatusPageView] controller.
+  ///
+  /// To stop animation on page scroll.
+  final PageController pageController;
+
+  @override
+  State<_StatusScreenDesktop> createState() => _StatusScreenDesktopState();
+}
+
+class _StatusScreenDesktopState extends State<_StatusScreenDesktop>
+    with SingleTickerProviderStateMixin {
+  /// Controller for status progress bar animation.
+  late final _controller = AnimationController(
+    vsync: this,
+    duration: const Duration(seconds: 5),
+  );
+
+  /// Notify when progress animation play or pause.
+  ///
+  /// To update the play/pause icon on appBar (desktop).
+  final _isAnimating = ValueNotifier<bool>(true);
+
+  @override
+  void initState() {
+    super.initState();
+    _controller.addStatusListener(_animationStatusListener);
+  }
+
+  late final _statusBloc = context.read<StatusBloc>();
+
+  /// Status page view controller.
+  late final _pageController = widget.pageController;
+
+  /// Show next status when animation completed.
+  void _animationStatusListener(AnimationStatus status) {
+    if (status == AnimationStatus.completed) _next();
+  }
+
+  /// Play/Continue the status progress animation.
+  void _play() {
+    _controller.forward();
+    _isAnimating.value = true;
+  }
+
+  /// Pause the status progress animation.
+  void _pause() {
+    _controller.stop();
+    _isAnimating.value = false;
+  }
+
+  /// Animate to next status page.
+  /// If it is the last status then pop the screen.
+  void _next() {
+    if (_pageController.page! == _statusBloc.state.statuses.length - 1) {
+      return Navigator.of(context).pop();
+    }
+
+    _pageController.nextPage(
+      duration: kTabScrollDuration,
+      curve: Curves.ease,
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.removeStatusListener(_animationStatusListener);
+    _controller.dispose();
+    _isAnimating.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      backgroundColor: Colors.black,
+      appBar: PreferredSize(
+        // +17 for status progress bar + top padding (2+15)
+        preferredSize: const Size.fromHeight(kToolbarHeight + 17),
+        child: _AppBar.desktop(
+          status: widget.status,
+          animation: _controller,
+          isAnimatingNotifier: _isAnimating,
+          play: _play,
+          pause: _pause,
+        ),
+      ),
+      body: GestureDetector(
+        behavior: HitTestBehavior.opaque,
+        onTapDown: (_) => _pause(),
+        onTapUp: (_) => _play(),
+
+        // Status image
+        child: Center(
+          child: VisibilityDetector(
+            key: ValueKey(widget.status.id),
+            onVisibilityChanged: (info) {
+              if (info.visibleFraction == 1) {
+                _play();
+                if (!widget.status.isSeen) {
+                  _statusBloc.add(StatusViewed(status: widget.status));
+                }
+              }
+            },
+            child: Image.network(widget.status.content.imgUrl!),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
 class _AppBar extends StatelessWidget {
-  const _AppBar({
+  const _AppBar.mobile({
     Key? key,
     required this.status,
     required this.animation,
-  }) : super(key: key);
+  })  : isAnimatingNotifier = null,
+        play = null,
+        pause = null,
+        super(key: key);
+
+  const _AppBar.desktop({
+    Key? key,
+    required this.status,
+    required this.animation,
+    required this.isAnimatingNotifier,
+    required this.play,
+    required this.pause,
+  })  : assert(isAnimatingNotifier != null && play != null && pause != null),
+        super(key: key);
 
   final Status status;
 
   /// Progress bar animation.
   final Animation<double> animation;
 
+  // Only on desktop
+  /// Notified when progress animation play or pause.
+  final ValueNotifier<bool>? isAnimatingNotifier;
+  final VoidCallback? play;
+  final VoidCallback? pause;
+
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      // Column with status progress bar and appBar
-      child: Column(
-        children: [
-          // Status progress
-          StatusProgressBar(animation: animation),
+    final isMobile = Theme.of(context).platform.isMobile;
+    // Column with status progress bar and appBar
+    final appBarWithProgressBar = Column(
+      children: [
+        // Status progress
+        StatusProgressBar(animation: animation),
 
-          AppBar(
-            // Elevation for text visibility on white screen
-            elevation: 0.1,
-            shadowColor: Colors.black26,
-            backgroundColor: Colors.transparent,
-            titleSpacing: 0,
-            title: Row(
-              children: [
-                UserDP(url: status.author.dpUrl),
-                const SizedBox(width: 10),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(status.author.name),
-                    Text(
-                      DateFormat(DateFormat.HOUR_MINUTE).format(status.time),
-                      style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                            color: Colors.white,
-                          ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
+        AppBar(
+          // Elevation for text visibility on white screen
+          elevation: 0.1,
+          shadowColor: Colors.black26,
+          backgroundColor: Colors.transparent,
+          titleSpacing: 0,
+          automaticallyImplyLeading: isMobile,
+          title: Row(
+            children: [
+              UserDP(url: status.author.dpUrl),
+              const SizedBox(width: 10),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(status.author.name),
+                  Text(
+                    DateFormat(DateFormat.HOUR_MINUTE).format(status.time),
+                    style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                          color: Colors.white,
+                        ),
+                  ),
+                ],
+              ),
+            ],
           ),
-        ],
+          actions: isMobile
+              ? null
+              : [
+                  // Play/Pause button
+                  IconButton(
+                    onPressed: () {
+                      if (isAnimatingNotifier!.value) return pause!();
+                      play!();
+                    },
+                    icon: ValueListenableBuilder<bool>(
+                      valueListenable: isAnimatingNotifier!,
+                      builder: (context, isAnimating, _) {
+                        return Icon(
+                          isAnimating ? Icons.pause : Icons.play_arrow,
+                        );
+                      },
+                    ),
+                  ),
+
+                  // Mute button
+                  const IconButton(
+                    onPressed: null,
+                    icon: Icon(Icons.volume_off),
+                  ),
+                ],
+        ),
+      ],
+    );
+
+    if (isMobile) return SafeArea(child: appBarWithProgressBar);
+
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.all(15).copyWith(bottom: 0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const BackButton(),
+
+            // AppBar with progress bar
+            Flexible(
+              child: SizedBox(
+                width: 600,
+                child: appBarWithProgressBar,
+              ),
+            ),
+
+            // Close button
+            IconButton(
+              icon: const Icon(Icons.close),
+              iconSize: 30,
+              onPressed: () {
+                // Pop the StatusScreen
+                Navigator.of(context).pop();
+                // Pop the StatusListView screen
+                context.read<StatusListViewCubit>().pop();
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
